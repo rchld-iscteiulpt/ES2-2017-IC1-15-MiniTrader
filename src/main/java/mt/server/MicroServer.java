b928ae5c52a6dafd7a0caa52b0ae8dc123e0d8b7
Roger <Roger@DESKTOP-8K2GRKR>
2017-05-16 23:44:47 +0100

    BR3 Ultimo

--- src/main/java/mt/server/MicroServer.java
+++ src/main/java/mt/server/MicroServer.java
@@ -104,7 +104,7 @@ public class MicroServer implements MicroTraderServer {
 						if(msg.getOrder().getServerOrderID() == EMPTY){
 							msg.getOrder().setServerOrderID(id++);
 						}
-						checkOrderUnits(msg.getOrder());
+						CheckMinimalOrderUnits(msg.getOrder());
 						notifyAllClients(msg.getOrder());
 						processNewOrder(msg);
 					} catch (ServerException e) {
@@ -368,8 +368,8 @@ public class MicroServer implements MicroTraderServer {
 		}
 	}
 	/*NEW CONDITION*/
-	private void checkOrderUnits(Order o) throws ServerException {
-				if (o.getNumberOfUnits() < 10) {
+	private void CheckMinimalOrderUnits(Order a) throws ServerException {
+				if (a.getNumberOfUnits() < 10) {
 					throw new ServerException("Order tem que ter no minimo 10 unidades");
 				}
 			}
